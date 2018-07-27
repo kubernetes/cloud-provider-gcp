198f9592878a9b0f70450245d8968ec018c8eab9
Andrew Lytvynov <awly@google.com>
2018-07-27 10:49:40 -0700

    Refactor fakeCA in ca_cache_test.go to reuse in TPM validation tests

--- cmd/gcp-controller-manager/app/ca_cache_test.go
+++ cmd/gcp-controller-manager/app/ca_cache_test.go
@@ -15,7 +15,6 @@ package app
 
 import (
 	"crypto"
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
@@ -25,37 +24,13 @@ import (
 	"path"
 	"testing"
 	"time"
+
+	"k8s.io/cloud-provider-gcp/pkg/nodeidentity"
 )
 
 func TestCACacheVerify(t *testing.T) {
-	var ca fakeCA
-	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		switch path.Base(r.URL.Path) {
-		case "root.crt":
-			rw.Write(ca.rootCert)
-		case "intermediate.crt":
-			rw.Write(ca.intermediateCert)
-		case "self-signed-intermediate.crt":
-			rw.Write(ca.selfSignedIntermediateCert)
-		case "root.crl":
-			rw.Write(ca.rootCRL)
-		case "intermediate.crl":
-			rw.Write(ca.intermediateCRL)
-		case "self-signed-intermediate.crl":
-			rw.Write(ca.selfSignedIntermediateCRL)
-		default:
-			http.Error(rw, "not found", http.StatusNotFound)
-		}
-	}))
-	defer srv.Close()
-	ca = initFakeCA(t, srv.URL)
-
-	c := &caCache{
-		rootCertURL: srv.URL + "/root.crt",
-		interPrefix: srv.URL,
-		certs:       make(map[string]*x509.Certificate),
-		crls:        make(map[string]*cachedCRL),
-	}
+	ca, c, cleanup := initFakeCACache(t)
+	defer cleanup()
 
 	t.Run("valid", func(t *testing.T) {
 		t.Parallel()
@@ -76,16 +51,54 @@ func TestCACacheVerify(t *testing.T) {
 	}
 }
 
-type fakeCA struct {
-	rootCert, intermediateCert, selfSignedIntermediateCert []byte
-	rootCRL, intermediateCRL, selfSignedIntermediateCRL    []byte
-	validCert                                              *x509.Certificate
-	invalidCerts                                           map[string]*x509.Certificate
+func initFakeCACache(t *testing.T) (*fakeCA, *caCache, func()) {
+	var ca *fakeCA
+	srv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		switch path.Base(r.URL.Path) {
+		case "root.crt":
+			rw.Write(ca.rootCert)
+		case "intermediate.crt":
+			rw.Write(ca.intermediateCertRaw)
+		case "self-signed-intermediate.crt":
+			rw.Write(ca.selfSignedIntermediateCert)
+		case "root.crl":
+			rw.Write(ca.rootCRL)
+		case "intermediate.crl":
+			rw.Write(ca.intermediateCRL)
+		case "self-signed-intermediate.crl":
+			rw.Write(ca.selfSignedIntermediateCRL)
+		default:
+			http.Error(rw, "not found", http.StatusNotFound)
+		}
+	}))
+
+	ca = initFakeCA(t, srv.URL)
+	c := &caCache{
+		rootCertURL: srv.URL + "/root.crt",
+		interPrefix: srv.URL,
+		certs:       make(map[string]*x509.Certificate),
+		crls:        make(map[string]*cachedCRL),
+	}
+	return ca, c, srv.Close
 }
 
-func initFakeCA(t *testing.T, srvURL string) fakeCA {
+type fakeCA struct {
+	rootCert, intermediateCertRaw, selfSignedIntermediateCert []byte
+	rootCRL, intermediateCRL, selfSignedIntermediateCRL       []byte
+	intermediateCert                                          *x509.Certificate
+	intermediateCertKey                                       *rsa.PrivateKey
+	validCert                                                 *x509.Certificate
+	validCertKey                                              *rsa.PrivateKey
+	invalidCerts                                              map[string]*x509.Certificate
+	srvURL                                                    string
+}
+
+func initFakeCA(t *testing.T, srvURL string) *fakeCA {
 	t.Helper()
-	ca := fakeCA{invalidCerts: make(map[string]*x509.Certificate)}
+	ca := &fakeCA{
+		invalidCerts: make(map[string]*x509.Certificate),
+		srvURL:       srvURL,
+	}
 
 	rootTmpl := &x509.Certificate{
 		SerialNumber: big.NewInt(1),
@@ -106,17 +119,9 @@ func initFakeCA(t *testing.T, srvURL string) fakeCA {
 		BasicConstraintsValid: true,
 		CRLDistributionPoints: []string{srvURL + "/root.crl"},
 	}
-	interCertDER, interCert, interKey := makeCert(t, interTmpl, rootCert, rootKey)
-	ca.intermediateCert = interCertDER
+	ca.intermediateCertRaw, ca.intermediateCert, ca.intermediateCertKey = makeCert(t, interTmpl, rootCert, rootKey)
 
-	_, ca.validCert, _ = makeCert(t, &x509.Certificate{
-		SerialNumber:          big.NewInt(3),
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(time.Hour),
-		KeyUsage:              x509.KeyUsageDigitalSignature,
-		IssuingCertificateURL: []string{srvURL + "/intermediate.crt"},
-		CRLDistributionPoints: []string{srvURL + "/intermediate.crl"},
-	}, interCert, interKey)
+	ca.regenerateValidCert(t, nodeidentity.Identity{"z0", 1, "i0", 2, "p0"})
 
 	_, ca.invalidCerts["revoked"], _ = makeCert(t, &x509.Certificate{
 		SerialNumber:          big.NewInt(4),
@@ -125,14 +130,14 @@ func initFakeCA(t *testing.T, srvURL string) fakeCA {
 		KeyUsage:              x509.KeyUsageDigitalSignature,
 		IssuingCertificateURL: []string{srvURL + "/intermediate.crt"},
 		CRLDistributionPoints: []string{srvURL + "/intermediate.crl"},
-	}, interCert, interKey)
+	}, ca.intermediateCert, ca.intermediateCertKey)
 	_, ca.invalidCerts["no CRL"], _ = makeCert(t, &x509.Certificate{
 		SerialNumber:          big.NewInt(4),
 		NotBefore:             time.Now(),
 		NotAfter:              time.Now().Add(time.Hour),
 		KeyUsage:              x509.KeyUsageDigitalSignature,
 		IssuingCertificateURL: []string{srvURL + "/intermediate.crt"},
-	}, interCert, interKey)
+	}, ca.intermediateCert, ca.intermediateCertKey)
 	selfSignedTmpl := &x509.Certificate{
 		SerialNumber: big.NewInt(99),
 		NotBefore:    time.Now(),
@@ -147,7 +152,7 @@ func initFakeCA(t *testing.T, srvURL string) fakeCA {
 		KeyUsage:              x509.KeyUsageDigitalSignature,
 		IssuingCertificateURL: []string{"http://example.com/wrong-intermediate.crt"},
 		CRLDistributionPoints: []string{srvURL + "/intermediate.crl"},
-	}, interCert, interKey)
+	}, ca.intermediateCert, ca.intermediateCertKey)
 
 	selfSignedInterTmpl := &x509.Certificate{
 		SerialNumber: big.NewInt(2),
@@ -171,18 +176,18 @@ func initFakeCA(t *testing.T, srvURL string) fakeCA {
 	_, ca.invalidCerts["self-signed intermediate"], _ = makeCert(t, wrongInterLeafTmpl, selfSignedInterCert, selfSignedInterKey)
 
 	var err error
-	ca.rootCRL, err = rootCert.CreateCRL(rand.Reader, rootKey, nil, time.Now(), time.Now())
+	ca.rootCRL, err = rootCert.CreateCRL(insecureRand, rootKey, nil, time.Now(), time.Now())
 	if err != nil {
 		t.Fatal(err)
 	}
-	ca.intermediateCRL, err = interCert.CreateCRL(rand.Reader, interKey, []pkix.RevokedCertificate{{
+	ca.intermediateCRL, err = ca.intermediateCert.CreateCRL(insecureRand, ca.intermediateCertKey, []pkix.RevokedCertificate{{
 		SerialNumber:   big.NewInt(4),
 		RevocationTime: time.Now(),
 	}}, time.Now(), time.Now())
 	if err != nil {
 		t.Fatal(err)
 	}
-	ca.selfSignedIntermediateCRL, err = selfSignedInterCert.CreateCRL(rand.Reader, selfSignedInterKey, nil, time.Now(), time.Now())
+	ca.selfSignedIntermediateCRL, err = selfSignedInterCert.CreateCRL(insecureRand, selfSignedInterKey, nil, time.Now(), time.Now())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -190,17 +195,36 @@ func initFakeCA(t *testing.T, srvURL string) fakeCA {
 	return ca
 }
 
-func makeCert(t *testing.T, tmpl, parent *x509.Certificate, parentKey crypto.PrivateKey) ([]byte, *x509.Certificate, crypto.PrivateKey) {
+func (ca *fakeCA) regenerateValidCert(t *testing.T, id nodeidentity.Identity) {
+	vmID, err := id.ToASN1()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, ca.validCert, ca.validCertKey = makeCert(t, &x509.Certificate{
+		SerialNumber:          big.NewInt(3),
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(time.Hour),
+		KeyUsage:              x509.KeyUsageDigitalSignature,
+		IssuingCertificateURL: []string{ca.srvURL + "/intermediate.crt"},
+		CRLDistributionPoints: []string{ca.srvURL + "/intermediate.crl"},
+		ExtraExtensions: []pkix.Extension{{
+			Id:    nodeidentity.CloudComputeInstanceIdentifierOID,
+			Value: vmID,
+		}},
+	}, ca.intermediateCert, ca.intermediateCertKey)
+}
+
+func makeCert(t *testing.T, tmpl, parent *x509.Certificate, parentKey crypto.PrivateKey) ([]byte, *x509.Certificate, *rsa.PrivateKey) {
 	t.Helper()
 
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	key, err := rsa.GenerateKey(insecureRand, 2048)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if parentKey == nil {
 		parentKey = key
 	}
-	certDER, err := x509.CreateCertificate(rand.Reader, tmpl, parent, key.Public(), parentKey)
+	certDER, err := x509.CreateCertificate(insecureRand, tmpl, parent, key.Public(), parentKey)
 	if err != nil {
 		t.Fatal(err)
 	}
