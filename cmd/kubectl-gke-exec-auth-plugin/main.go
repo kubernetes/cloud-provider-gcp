22531c672af7544e287f6ead0e0470ae400a9bd8
Kiran Tumkur <kirantumkur@google.com>
2021-06-21 16:25:40 -0700

    Add versioning to kubectl-gke-exec-plugin binary.

--- cmd/kubectl-gke-exec-auth-plugin/main.go
+++ cmd/kubectl-gke-exec-auth-plugin/main.go
@@ -4,10 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/spf13/pflag"
 	"k8s.io/cloud-provider-gcp/cmd/kubectl-gke-exec-auth-plugin/provider"
+	"k8s.io/component-base/version/verflag"
 )
 
 func main() {
+	pflag.Parse()
+	verflag.PrintAndExitIfRequested()
+
 	ec, err := provider.ExecCredential()
 	if err != nil {
 		msg := fmt.Errorf("unable to retrieve access token for GKE. Error : %v", err)
