cf2335c67c143517d6c22ab7781360e59c002341
Aaron Crickenberger <spiffxp@google.com>
2021-07-19 07:28:22 -0700

    use dl.k8s.io where possible
    The only time a kubernetes GCS bucket name should be showing up in a
    hardcoded URI is if gsutil is being used (e.g. gsutil cp gs://foo/bar .)
    
    Otherwise, for tools like curl or wget, dl.k8s.io is much nicer for us
    as a project, since we can transparently change where that redirects to
    without having to change code everywhere
    
    These changes will mean very few changes will be necessary to
    accommodate a gs://kubernetes-release -> gs://k8s-release migration
    equivalent of the CI migration we're going through right now
    
    These changes also address the gs://kubernetes-release-dev references
    currently used by this repo (ref:
    https://github.com/kubernetes/k8s.io/issues/2318)

--- cluster/gce/upgrade.sh
+++ cluster/gce/upgrade.sh
@@ -61,9 +61,9 @@ function usage() {
   local release_latest
   local ci_latest
 
-  release_stable=$(gsutil cat gs://kubernetes-release/release/stable.txt)
-  release_latest=$(gsutil cat gs://kubernetes-release/release/latest.txt)
-  ci_latest=$(gsutil cat gs://kubernetes-release-dev/ci/latest.txt)
+  release_stable=$(curl -Ls https://dl.k8s.io/release/stable.txt)
+  release_latest=$(curl -Ls https://dl.k8s.io/release/latest.txt)
+  ci_latest=$(curl -Ls https://dl.k8s.io/ci/latest.txt)
 
   echo "Right now, versions are as follows:"
   echo "  release/stable: ${0} ${release_stable}"
