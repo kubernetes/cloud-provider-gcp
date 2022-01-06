5fe18b6d7bed0d47cad1cb3343f52c0d23a61580
acumino <sksgkpvks@gmail.com>
2022-01-06 10:16:06 +0530

    Address PR reviews

--- cmd/gcp-controller-manager/main.go
+++ cmd/gcp-controller-manager/main.go
@@ -87,7 +87,7 @@ func main() {
 		LeaseDuration: metav1.Duration{Duration: 15 * time.Second},
 		RenewDeadline: metav1.Duration{Duration: 10 * time.Second},
 		RetryPeriod:   metav1.Duration{Duration: 2 * time.Second},
-		//TODO(acumino): Switch endpointsleases to leases in one of the future version.
+		//TODO(acumino): Migrate endpointsleases to leases in vesrion 1.24.
 		ResourceLock: rl.EndpointsLeasesResourceLock,
 	}
 	options.BindLeaderElectionFlags(leConfig, pflag.CommandLine)
