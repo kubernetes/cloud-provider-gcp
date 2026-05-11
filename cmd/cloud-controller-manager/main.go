kubernetes/cloud-provider-gcp

commit d4f479ebe9f33c1706a1d5b1cfafe903c23dc252

feat: Skip managing non-GKE nodes with a special label (#1081)
* Override node lifecycle manager to filter nodes by cloud.google.com/gke-unmanaged-node=true label

* Fix crash in node lifecycle controller by hardcoding client name

* Refactor node filtering logic to ignore unmanaged nodes in Lifecycle and IPAM controllers

- Centralized unmanaged node filtering logic into a shared utility in pkg/util/node/node.go.
- Updated Node Lifecycle Controller to use the shared filtering utility.
- Updated Node IPAM Controller (both Cloud and Range allocators) to ignore nodes with the cloud.google.com/gke-unmanaged-node: true label.
- Updated related tests to reflect the changes.

* InstanceExists

* Fix CloudNodeController to ignore unmanaged nodes

- Overrode the default CloudNodeController initializer in cmd/cloud-controller-manager/main.go.
- Implemented startCloudNodeControllerWrapper in a new file cmd/cloud-controller-manager/nodecontroller.go.
- The new initializer wraps the node informer with GCEFilteringNodeInformer to ensure the node controller filters out nodes with the cloud.google.com/gke-unmanaged-node: true label.

* Fix ipam missing node

* Remove filter in New func

* update the code after rebase

* update the log message

* rename the label

* fmt

* add comments

* Create dedicated error for not managed nodes

* move the filtering informer to top level of ipam controller

* update the unit test

* unified the node controller and node lifecycle controller

* fix a error in err handing

* Remove filtering informer

* Implement event stream filtering in GCEFilteringNodeInformer and clean up redundant checks in range_allocator

* nit: rename some variables

cmd/cloud-controller-manager/main.go
@@ -111,6 +111,17 @@ func main() {
 		Constructor: nodeIpamController.startNodeIpamControllerWrapper,
 	}
 
+	controllerInitializers[names.CloudNodeController] = app.ControllerInitFuncConstructor{
+		InitContext: app.ControllerInitContext{
+			ClientName: "node-controller",
+		},
+		Constructor: startCloudNodeControllerWrapper,
+	}
+
+	controllerInitializers[names.CloudNodeLifecycleController] = app.ControllerInitFuncConstructor{
+		Constructor: startCloudNodeLifecycleControllerWrapper,
+	}
+
 	controllerInitializers["gkenetworkparamset"] = app.ControllerInitFuncConstructor{
 		Constructor: startGkeNetworkParamSetControllerWrapper,
 	}

cmd/cloud-controller-manager/nodelifecyclecontroller.go
@@ -1,0 +1,58 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+
+	cloudprovider "k8s.io/cloud-provider"
+	utilnode "k8s.io/cloud-provider-gcp/pkg/util/node"
+	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/controllers/nodelifecycle"
+	controllermanagerapp "k8s.io/controller-manager/app"
+	"k8s.io/controller-manager/controller"
+	"k8s.io/klog/v2"
+)
+
+func startCloudNodeLifecycleControllerWrapper(initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) app.InitFunc {
+	return func(ctx context.Context, controllerContext controllermanagerapp.ControllerContext) (controller.Interface, bool, error) {
+		return startCloudNodeLifecycleController(ctx, initContext, controllerContext, completedConfig, cloud)
+	}
+}
+
+func startCloudNodeLifecycleController(ctx context.Context, initContext app.ControllerInitContext, controllerContext controllermanagerapp.ControllerContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (controller.Interface, bool, error) {
+	// Wrap the informer to filter nodes
+	filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: completedConfig.SharedInformers.Core().V1().Nodes()}
+
+	// Start the cloudNodeLifecycleController
+	cloudNodeLifecycleController, err := nodelifecycle.NewCloudNodeLifecycleController(
+		filteringInformer,
+		// cloud node lifecycle controller uses existing cluster role from node-controller
+		completedConfig.ClientBuilder.ClientOrDie("cloud-node-lifecycle-controller"),
+		cloud,
+		completedConfig.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration,
+	)
+	if err != nil {
+		klog.Warningf("failed to start cloud node lifecycle controller: %s", err)
+		return nil, false, nil
+	}
+
+	go cloudNodeLifecycleController.Run(ctx, controllerContext.ControllerManagerMetrics)
+
+	return nil, true, nil
+}

cmd/cloud-controller-manager/gketenantcontrollermanager.go
@@ -20,6 +20,7 @@ import (
 	"k8s.io/cloud-provider-gcp/pkg/controller/gketenantcontrollers"
 	"k8s.io/cloud-provider-gcp/pkg/controller/gketenantcontrollers/utils"
 	nodeipamconfig "k8s.io/cloud-provider-gcp/pkg/controller/nodeipam/config"
+	utilnode "k8s.io/cloud-provider-gcp/pkg/util/node"
 	"k8s.io/cloud-provider/app"
 	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
 	controllermanagerapp "k8s.io/controller-manager/app"
@@ -104,8 +105,10 @@ func startGKETenantControllerManager(mgrCfg gkeTenantControllerManagerConfig) (c
 	controllers := map[string]gketenantcontrollers.ControllerStartFunc{
 		"node-controller": func(cfg *gketenantcontrollers.ControllerConfig) error {
 			klog.Infof("Creating OSS Cloud Node Controller for %s...", cfg.ProviderConfig.Name)
+			// Wrap the informer to filter nodes
+			filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: cfg.NodeInformer}
 			nodeController, err := node.NewCloudNodeController(
-				cfg.NodeInformer,
+				filteringInformer,
 				cfg.KubeClient,
 				cfg.Cloud,
 				mgrCfg.completedConfig.ComponentConfig.NodeStatusUpdateFrequency.Duration,
@@ -133,9 +136,12 @@ func startGKETenantControllerManager(mgrCfg gkeTenantControllerManagerConfig) (c
 				tenantNodeIPAMConfig.EnableMultiSubnetCluster = false
 			}
 
+			// Wrap the informer to filter nodes
+			filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: cfg.NodeInformer}
+
 			_, started, err := nodeipam.StartNodeIpamController(
 				cfg.Context,
-				cfg.NodeInformer,
+				filteringInformer,
 				cfg.KubeClient,
 				cfg.Cloud,
 				cidrs,
@@ -172,8 +178,10 @@ func startGKETenantControllerManager(mgrCfg gkeTenantControllerManagerConfig) (c
 		"node-lifecycle-controller": func(cfg *gketenantcontrollers.ControllerConfig) error {
 			klog.Infof("Creating Node Lifecycle Controller for %s...", cfg.ProviderConfig.Name)
 			nodeMonitorPeriod := mgrCfg.completedConfig.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration
+			// Wrap the informer to filter nodes
+			filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: cfg.NodeInformer}
 			lifecycleController, err := nodelifecycle.NewCloudNodeLifecycleController(
-				cfg.NodeInformer,
+				filteringInformer,
 				cfg.KubeClient,
 				cfg.Cloud,
 				nodeMonitorPeriod,

cmd/cloud-controller-manager/nodecontroller.go
@@ -1,0 +1,60 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+
+	cloudprovider "k8s.io/cloud-provider"
+	utilnode "k8s.io/cloud-provider-gcp/pkg/util/node"
+	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/controllers/node"
+	controllermanagerapp "k8s.io/controller-manager/app"
+	"k8s.io/controller-manager/controller"
+	"k8s.io/klog/v2"
+)
+
+func startCloudNodeControllerWrapper(initContext app.ControllerInitContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) app.InitFunc {
+	return func(ctx context.Context, controllerContext controllermanagerapp.ControllerContext) (controller.Interface, bool, error) {
+		return startCloudNodeController(ctx, initContext, controllerContext, completedConfig, cloud)
+	}
+}
+
+func startCloudNodeController(ctx context.Context, initContext app.ControllerInitContext, controllerContext controllermanagerapp.ControllerContext, completedConfig *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface) (controller.Interface, bool, error) {
+	// Wrap the informer to filter nodes
+	filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: completedConfig.SharedInformers.Core().V1().Nodes()}
+
+	// Start the CloudNodeController
+	nodeController, err := node.NewCloudNodeController(
+		filteringInformer,
+		// cloud node controller uses existing cluster role from node-controller
+		completedConfig.ClientBuilder.ClientOrDie(initContext.ClientName),
+		cloud,
+		completedConfig.ComponentConfig.NodeStatusUpdateFrequency.Duration,
+		completedConfig.ComponentConfig.NodeController.ConcurrentNodeSyncs,
+		completedConfig.ComponentConfig.NodeController.ConcurrentNodeStatusUpdates,
+	)
+	if err != nil {
+		klog.Warningf("failed to start cloud node controller: %s", err)
+		return nil, false, nil
+	}
+
+	go nodeController.Run(ctx.Done(), controllerContext.ControllerManagerMetrics)
+
+	return nil, true, nil
+}

cmd/cloud-controller-manager/nodeipamcontroller.go
@@ -29,6 +29,7 @@ import (
 	nodeipamcontroller "k8s.io/cloud-provider-gcp/pkg/controller/nodeipam"
 	nodeipamconfig "k8s.io/cloud-provider-gcp/pkg/controller/nodeipam/config"
 	"k8s.io/cloud-provider-gcp/pkg/controller/nodeipam/ipam"
+	utilnode "k8s.io/cloud-provider-gcp/pkg/util/node"
 	"k8s.io/cloud-provider/app"
 	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
 	genericcontrollermanager "k8s.io/controller-manager/app"
@@ -70,9 +71,12 @@ func startNodeIpamController(ccmConfig *cloudcontrollerconfig.CompletedConfig, n
 	nwInformer := nwInfFactory.Networking().V1().Networks()
 	gnpInformer := nwInfFactory.Networking().V1().GKENetworkParamSets()
 
+	// Wrap the informer to filter nodes
+	filteringInformer := &utilnode.GKEFilteringNodeInformer{NodeInformer: ccmConfig.SharedInformers.Core().V1().Nodes()}
+
 	ctrl, started, err := nodeipamcontroller.StartNodeIpamController(
 		wait.ContextForChannel(ctx.Stop),
-		ccmConfig.SharedInformers.Core().V1().Nodes(),
+		filteringInformer,
 		ccmConfig.ClientBuilder.ClientOrDie("node-controller"),
 		cloud,
 		ccmConfig.ComponentConfig.KubeCloudShared.ClusterCIDR,

cmd/cloud-controller-manager/nodelifecyclecontroller_test.go
@@ -1,0 +1,116 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
+	cloudprovider "k8s.io/cloud-provider"
+	"k8s.io/cloud-provider-gcp/pkg/util/node"
+	"k8s.io/cloud-provider/controllers/nodelifecycle"
+)
+
+type mockInstances struct {
+	cloudprovider.Instances
+	existsCalled map[string]bool
+}
+
+func (m *mockInstances) InstanceExistsByProviderID(ctx context.Context, providerID string) (bool, error) {
+	if m.existsCalled == nil {
+		m.existsCalled = make(map[string]bool)
+	}
+	m.existsCalled[providerID] = true
+	return true, nil
+}
+
+func (m *mockInstances) InstanceShutdownByProviderID(ctx context.Context, providerID string) (bool, error) {
+	return false, nil
+}
+
+type mockCloud struct {
+	cloudprovider.Interface
+	instances *mockInstances
+}
+
+func (m *mockCloud) Instances() (cloudprovider.Instances, bool) {
+	return m.instances, true
+}
+
+func (m *mockCloud) InstancesV2() (cloudprovider.InstancesV2, bool) {
+	return nil, false
+}
+
+func TestMonitorNodes_FilterLabel(t *testing.T) {
+	fakeClient := fake.NewSimpleClientset()
+	informerFactory := informers.NewSharedInformerFactory(fakeClient, 0)
+	nodeInformer := informerFactory.Core().V1().Nodes()
+
+	managedNode := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{Name: "managed-node"},
+		Spec:       v1.NodeSpec{ProviderID: "gce://project/zone/managed-node"},
+		Status:     v1.NodeStatus{Conditions: []v1.NodeCondition{{Type: v1.NodeReady, Status: v1.ConditionFalse}}},
+	}
+	unmanagedNode := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "unmanaged-node",
+			Labels: map[string]string{node.GKEUnmanagedNodeLabelKey: node.GKEUnmanagedNodeLabelValue},
+		},
+		Spec:   v1.NodeSpec{ProviderID: "gce://project/zone/unmanaged-node"},
+		Status: v1.NodeStatus{Conditions: []v1.NodeCondition{{Type: v1.NodeReady, Status: v1.ConditionFalse}}},
+	}
+	labeledManagedNode := &v1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "labeled-managed-node",
+			Labels: map[string]string{node.GKEUnmanagedNodeLabelKey: "false"},
+		},
+		Spec:   v1.NodeSpec{ProviderID: "gce://project/zone/labeled-managed-node"},
+		Status: v1.NodeStatus{Conditions: []v1.NodeCondition{{Type: v1.NodeReady, Status: v1.ConditionFalse}}},
+	}
+
+	nodeInformer.Informer().GetStore().Add(managedNode)
+	nodeInformer.Informer().GetStore().Add(unmanagedNode)
+	nodeInformer.Informer().GetStore().Add(labeledManagedNode)
+
+	mockInst := &mockInstances{existsCalled: make(map[string]bool)}
+	mockCl := &mockCloud{instances: mockInst}
+
+	// Wrap the informer to filter nodes
+	filteringInformer := &node.GKEFilteringNodeInformer{NodeInformer: nodeInformer}
+
+	c, err := nodelifecycle.NewCloudNodeLifecycleController(filteringInformer, fakeClient, mockCl, 5*time.Minute)
+	if err != nil {
+		t.Fatalf("failed to create controller: %v", err)
+	}
+
+	c.MonitorNodes(context.Background())
+
+	if !mockInst.existsCalled[managedNode.Spec.ProviderID] {
+		t.Errorf("expected InstanceExistsByProviderID to be called for managed node, but it wasn't")
+	}
+	if !mockInst.existsCalled[labeledManagedNode.Spec.ProviderID] {
+		t.Errorf("expected InstanceExistsByProviderID to be called for labeled managed node (false), but it wasn't")
+	}
+	if mockInst.existsCalled[unmanagedNode.Spec.ProviderID] {
+		t.Errorf("expected InstanceExistsByProviderID NOT to be called for unmanaged node, but it was")
+	}
+}

pkg/controller/nodeipam/ipam/cloud_cidr_allocator_test.go
@@ -510,7 +510,9 @@ func TestUpdateCIDRAllocation(t *testing.T) {
 				},
 				Clientset: fake.NewSimpleClientset(),
 			},
-			nodeChanges: func(node *v1.Node) {},
+			nodeChanges:  func(node *v1.Node) {},
+			expectErr:    true,
+			expectErrMsg: "node \"test\" not found",
 		},
 		{
 			name: "want error - provider not set",

pkg/util/node/node.go
@@ -25,8 +25,12 @@ import (
 	networkv1 "github.com/GoogleCloudPlatform/gke-networking-api/apis/network/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
+	coreinformers "k8s.io/client-go/informers/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
+	v1lister "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 )
 
@@ -39,7 +43,122 @@ const (
 	DefaultSubnetLabelPrefix = "cloud.google.com/gke-np-default-subnet"
 	// NodePoolSubnetLabelPrefix is the prefix for the subnet name for this node
 	NodePoolSubnetLabelPrefix = "cloud.google.com/gke-node-pool-subnet"
+	// GKEUnmanagedNodeLabelKey is the label key used to identify nodes that should be ignored by GKE controllers.
+	GKEUnmanagedNodeLabelKey = "components.gke.io/gke-unmanaged-node"
+	// GKEUnmanagedNodeLabelValue is the label value used to identify nodes that should be ignored by GKE controllers.
+	GKEUnmanagedNodeLabelValue = "true"
 )
+
+// GKEFilteringNodeInformer wraps a NodeInformer to filter out unmanaged nodes from the lister.
+type GKEFilteringNodeInformer struct {
+	coreinformers.NodeInformer
+}
+
+// Lister returns a GKEFilteringNodeLister.
+func (i *GKEFilteringNodeInformer) Lister() v1lister.NodeLister {
+	return &GKEFilteringNodeLister{i.NodeInformer.Lister()}
+}
+
+// Informer returns the wrapped Informer.
+func (i *GKEFilteringNodeInformer) Informer() cache.SharedIndexInformer {
+	return &GKEFilteringSharedIndexInformer{i.NodeInformer.Informer()}
+}
+
+// GKEFilteringSharedIndexInformer wraps a SharedIndexInformer to filter out unmanaged nodes from the event stream.
+type GKEFilteringSharedIndexInformer struct {
+	cache.SharedIndexInformer
+}
+
+// AddEventHandler adds an event handler to the shared informer with filtering.
+func (f *GKEFilteringSharedIndexInformer) AddEventHandler(handler cache.ResourceEventHandler) (cache.ResourceEventHandlerRegistration, error) {
+	return f.SharedIndexInformer.AddEventHandler(cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj interface{}) bool {
+			node, ok := obj.(*v1.Node)
+			return ok && !IsUnmanagedNode(node)
+		},
+		Handler: handler,
+	})
+}
+
+// AddEventHandlerWithResyncPeriod adds an event handler to the shared informer with a custom resync period and filtering.
+func (f *GKEFilteringSharedIndexInformer) AddEventHandlerWithResyncPeriod(handler cache.ResourceEventHandler, resyncPeriod time.Duration) (cache.ResourceEventHandlerRegistration, error) {
+	return f.SharedIndexInformer.AddEventHandlerWithResyncPeriod(cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj interface{}) bool {
+			node, ok := obj.(*v1.Node)
+			return ok && !IsUnmanagedNode(node)
+		},
+		Handler: handler,
+	}, resyncPeriod)
+}
+
+// IsUnmanagedNode returns true if the node has the unmanaged label.
+func IsUnmanagedNode(node *v1.Node) bool {
+	if node == nil {
+		return false
+	}
+	val, ok := node.Labels[GKEUnmanagedNodeLabelKey]
+	return ok && val == GKEUnmanagedNodeLabelValue
+}
+
+// GKEFilteringNodeLister wraps a NodeLister to filter out nodes with the unmanaged label.
+type GKEFilteringNodeLister struct {
+	v1lister.NodeLister
+}
+
+// List lists all Nodes in the indexer, filtering out unmanaged nodes.
+func (l *GKEFilteringNodeLister) List(selector labels.Selector) (ret []*v1.Node, err error) {
+	nodes, err := l.NodeLister.List(selector)
+	if err != nil {
+		return nil, err
+	}
+	var filtered []*v1.Node
+	for _, n := range nodes {
+		if IsUnmanagedNode(n) {
+			continue
+		}
+		filtered = append(filtered, n)
+	}
+	return filtered, nil
+}
+
+// Get retrieves a Node by name from the indexer, returning a ErrNodeUnmanaged error if the node is unmanaged.
+func (l *GKEFilteringNodeLister) Get(name string) (*v1.Node, error) {
+	node, err := l.NodeLister.Get(name)
+	if err != nil {
+		return nil, err
+	}
+	if IsUnmanagedNode(node) {
+		return nil, &ErrNodeUnmanagedByGKE{Name: name}
+	}
+	return node, nil
+}
+
+// ErrNodeUnmanagedByGKE is returned when a node is found but marked as unmanaged.
+type ErrNodeUnmanagedByGKE struct {
+	Name string
+}
+
+func (e *ErrNodeUnmanagedByGKE) Error() string {
+	return fmt.Sprintf("node %q is unmanaged by GKE", e.Name)
+}
+
+// Status allows this error to be recognized by apierrors.IsNotFound()
+// so that standard controllers ignore it instead of retrying forever.
+func (e *ErrNodeUnmanagedByGKE) Status() metav1.Status {
+	return metav1.Status{
+		Reason:  metav1.StatusReasonNotFound,
+		Message: e.Error(),
+	}
+}
+
+// IsUnmanagedNodeError checks if an error is an ErrNodeUnmanaged.
+func IsUnmanagedNodeError(err error) bool {
+	if err == nil {
+		return false
+	}
+	_, ok := err.(*ErrNodeUnmanagedByGKE)
+	return ok
+}
 
 type nodeForConditionPatch struct {
 	Status nodeStatusForPatch `json:"status"`

commit 7cdc936e219ea53410b48d0a0da4c728ce1aa092

fix: Ensure NodeLifecycleController has correct permissions
This commit fixes a regression introduced in https://github.com/kubernetes/cloud-provider-gcp/pull/1081 where nodes could not be
deleted after their underlying GCE instances were removed.

- Fixed a client identity mismatch in the NodeLifecycleController. It was using
  a hardcoded client name instead of the configured 'node-controller' identity,
  which lacks the necessary RBAC permissions for node deletion in GKE.

Tested:
1. Delete a GKE nodepool
   - Before the fix: the node object is still in the api-server
   - After this fix: the node object is removed

cmd/cloud-controller-manager/main.go
@@ -119,6 +119,9 @@ func main() {
 	}
 
 	controllerInitializers[names.CloudNodeLifecycleController] = app.ControllerInitFuncConstructor{
+		InitContext: app.ControllerInitContext{
+			ClientName: "node-controller",
+		},
 		Constructor: startCloudNodeLifecycleControllerWrapper,
 	}
 

cmd/cloud-controller-manager/nodelifecyclecontroller.go
@@ -43,7 +43,7 @@ func startCloudNodeLifecycleController(ctx context.Context, initContext app.Cont
 	cloudNodeLifecycleController, err := nodelifecycle.NewCloudNodeLifecycleController(
 		filteringInformer,
 		// cloud node lifecycle controller uses existing cluster role from node-controller
-		completedConfig.ClientBuilder.ClientOrDie("cloud-node-lifecycle-controller"),
+		completedConfig.ClientBuilder.ClientOrDie(initContext.ClientName),
 		cloud,
 		completedConfig.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration,
 	)