3dcf2b40fa4e28684336bd8091f30b3f6b682533
LogicalShark <maralder@google.com>
2026-05-14 18:26:13 +0000

    Handle filenames with whitespace in gofmt.sh
    Signed-off-by: LogicalShark <maralder@google.com>

--- tools/verify-gofmt.sh
+++ tools/verify-gofmt.sh
@@ -36,14 +36,14 @@ find_files() {
         -o -wholename './release' \
         -o -wholename '*/vendor/*' \
       \) -prune \
-    \) -name '*.go'
+    \) -name '*.go' -print0
 }
 
 # gofmt exits with non-zero exit code if it finds a problem unrelated to
 # formatting (e.g., a file does not parse correctly). Without "|| true" this
 # would have led to no useful error message from gofmt, because the script would
 # have failed before getting to the "echo" in the block below.
-diff=$(find_files | xargs "${gofmt}" -d -s 2>&1) || true
+diff=$(find_files | xargs -0 "${gofmt}" -d -s 2>&1) || true
 if [[ -n "${diff}" ]]; then
   echo "${diff}" >&2
   echo >&2
