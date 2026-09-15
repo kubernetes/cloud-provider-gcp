602ad9ef73c7a118e991437d9aca692bc52b1648
Zuzanna Nowak <zuzannanowak@google.com>
2026-09-15 18:17:19 +0200

    refactor IPv6 PodCIDRs assertions tests (#1353)

--- pkg/controller/gkenetworkparamset/gkenetworkparamset_controller_test.go
+++ pkg/controller/gkenetworkparamset/gkenetworkparamset_controller_test.go
@@ -445,58 +445,84 @@ func TestValidParamSetSubnetRange(t *testing.T) {
 
 }
 
-func TestValidParamSetSubnetInternalIpv6Prefix(t *testing.T) {
-	g := gomega.NewGomegaWithT(t)
-	ctx, stop := context.WithCancel(context.Background())
-	defer stop()
-	testVals := setupGKENetworkParamSetController(ctx, defaultPodIPv6CIDR)
-
+func TestValidParamSetSubnetIpv6Prefixes(t *testing.T) {
 	subnetName := "ipv6-subnet"
 	subnetIpv6Cidr := "2001:db8::/32"
-	subnetKey := meta.RegionalKey(subnetName, testVals.clusterValues.Region)
 
-	subnet := &compute.Subnetwork{
-		Name:               subnetName,
-		InternalIpv6Prefix: subnetIpv6Cidr,
-	}
-
-	err := testVals.cloud.Compute().Subnetworks().Insert(ctx, subnetKey, subnet)
-	if err != nil {
-		t.Error(err)
-	}
-	testVals.runGKENetworkParamSetController(ctx)
-
-	gkeNetworkParamSetName := "test-paramset-ipv6"
-	paramSet := &networkv1.GKENetworkParamSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: gkeNetworkParamSetName,
+	testCases := []struct {
+		name   string
+		subnet *compute.Subnetwork
+	}{
+		{
+			name: "InternalIpv6Prefix",
+			subnet: &compute.Subnetwork{
+				Name:               subnetName,
+				InternalIpv6Prefix: subnetIpv6Cidr,
+			},
 		},
-		Spec: networkv1.GKENetworkParamSetSpec{
-			VPC:        nonDefaultTestNetworkName,
-			VPCSubnet:  subnetName,
-			DeviceMode: networkv1.NetDevice,
+		{
+			name: "ExternalIpv6Prefix",
+			subnet: &compute.Subnetwork{
+				Name:               subnetName,
+				ExternalIpv6Prefix: subnetIpv6Cidr,
+			},
+		},
+		{
+			name: "LegacyIpv6CidrRange",
+			subnet: &compute.Subnetwork{
+				Name:          subnetName,
+				Ipv6CidrRange: subnetIpv6Cidr,
+			},
 		},
 	}
 
-	_, err = testVals.networkClient.NetworkingV1().GKENetworkParamSets().Create(ctx, paramSet, metav1.CreateOptions{})
-	if err != nil {
-		t.Error(err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := gomega.NewGomegaWithT(t)
+			ctx, stop := context.WithCancel(context.Background())
+			defer stop()
+
+			testVals := setupGKENetworkParamSetController(ctx, defaultPodIPv6CIDR)
+			subnetKey := meta.RegionalKey(tc.subnet.Name, testVals.clusterValues.Region)
+
+			err := testVals.cloud.Compute().Subnetworks().Insert(ctx, subnetKey, tc.subnet)
+			if err != nil {
+				t.Error(err)
+			}
+
+			testVals.runGKENetworkParamSetController(ctx)
+
+			gkeNetworkParamSetName := "test-paramset-ipv6"
+			paramSet := &networkv1.GKENetworkParamSet{
+				ObjectMeta: metav1.ObjectMeta{Name: gkeNetworkParamSetName},
+				Spec: networkv1.GKENetworkParamSetSpec{
+					VPC:        nonDefaultTestNetworkName,
+					VPCSubnet:  tc.subnet.Name,
+					DeviceMode: networkv1.NetDevice,
+				},
+			}
+
+			_, err = testVals.networkClient.NetworkingV1().GKENetworkParamSets().Create(ctx, paramSet, metav1.CreateOptions{})
+			if err != nil {
+				t.Error(err)
+			}
+
+			g.Eventually(func() (bool, error) {
+				paramSet, err := testVals.networkClient.NetworkingV1().GKENetworkParamSets().Get(ctx, gkeNetworkParamSetName, metav1.GetOptions{})
+				if err != nil {
+					return false, err
+				}
+
+				validCond := condmeta.FindStatusCondition(paramSet.Status.Conditions, string(networkv1.GKENetworkParamSetStatusReady))
+				if validCond != nil && validCond.Status == metav1.ConditionTrue {
+					g.Ω(paramSet.Status.PodCIDRs).Should(gomega.BeNil())
+					return true, nil
+				}
+
+				return false, nil
+			}).Should(gomega.BeTrue(), "GKENetworkParamSet Status PodCIDRs should be nil for IPv6 subnet.")
+		})
 	}
-
-	g.Eventually(func() (bool, error) {
-		paramSet, err := testVals.networkClient.NetworkingV1().GKENetworkParamSets().Get(ctx, gkeNetworkParamSetName, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-
-		validCond := condmeta.FindStatusCondition(paramSet.Status.Conditions, string(networkv1.GKENetworkParamSetStatusReady))
-		if validCond != nil && validCond.Status == metav1.ConditionTrue {
-			g.Ω(paramSet.Status.PodCIDRs).Should(gomega.BeNil())
-			return true, nil
-		}
-
-		return false, nil
-	}).Should(gomega.BeTrue(), "GKENetworkParamSet Status should be updated ONLY with subnet internal ipv6 prefix.")
 }
 
 func TestAddAndRemoveFinalizerToGKENetworkParamSet_NoNetworkName(t *testing.T) {
